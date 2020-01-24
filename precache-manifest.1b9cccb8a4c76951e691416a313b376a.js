33ee9c6990f7874950514dcc8240079c38bb89d8
zombierust <zombierust@hotmail.com>
2020-01-24 17:33:49 +0000

    Updates

---
+++ precache-manifest.1b9cccb8a4c76951e691416a313b376a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "262a440bbe80b9107faf4ef398df2a91",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d8e1d18065943415162f",
+    "url": "/monsters-rolodex/static/css/main.41dc7008.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d8e1d18065943415162f",
+    "url": "/monsters-rolodex/static/js/main.e5cf2a65.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
